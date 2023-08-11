16d321ec2d2f56501a9ebe3ccc64d3a0d53cd667
JamesBond <introbond.dev@gmail.com>
2023-08-11 08:49:15 +0700

    SWD-report temp

--- src/components/imageData.js
+++ src/components/imageData.js
@@ -1,7 +1,4 @@
-import p1 from "../assets/api-result.png"
-import p2 from "../assets/report-after.png"
-import p3 from "../assets/report-before.png"
-import p4 from "../assets/report-result.png"
+import p1 from "../assets/report.png"
 
 class cirtification {
     constructor(name, image, description, date) {
@@ -14,27 +11,9 @@ class cirtification {
 
 export const ImageData = [
     new cirtification(
-        "Alerted devices API result.", 
+        "", 
         p1, 
-        "Alerted devices API result.", 
+        "Fix: device status & battery logic", 
         "2022-10-18"
-        ),
-    new cirtification(
-        "Report downloading before optimize runtime (45 days data size projection on InfluxDB).", 
-        p3, 
-        "Report downloading before optimize runtime (45 days data size projection on InfluxDB).", 
-        "2022-10-19"
-        ),
-    new cirtification(
-        "Report downloading after optimize runtime (45 days data size projection on InfluxDB).", 
-        p2, 
-        "Report downloading after optimize runtime (45 days data size projection on InfluxDB).", 
-        "2022-10-20"
-        ),
-    new cirtification(
-        "Report result", 
-        p4, 
-        "Report result", 
-        "2022-10-21"
         ),
 ]
