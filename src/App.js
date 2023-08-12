3ef06d6ea7b0e70af31be23dc61219ded7ec46ab
JamesBond <introbond.dev@gmail.com>
2023-08-12 10:54:53 +0700

    Update: Auto play image slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ImageSlider from './components/ImageSlider';
+import SwipeableTextMobileStepper from './components/ImageSlider2';
 
 function App() {
   return (
     <div className="App">
-      <ImageSlider/>
+      {/* <ImageSlider/> */}
+      <SwipeableTextMobileStepper/>
     </div>
   );
 }
