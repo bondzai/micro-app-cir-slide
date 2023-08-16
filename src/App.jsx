16046690d4e8675dc8f933ec56fe861adcabff00
JamesBond <introbond.dev@gmail.com>
2023-08-16 08:21:38 +0700

    Update: SWD temp files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import ImageSlider from './components/ImageSlider';
-import SwipeableTextMobileStepper from './components/ImageSlider2';
 
 function App() {
   return (
     <div className="App">
-      {/* <ImageSlider/> */}
-      <SwipeableTextMobileStepper/>
+      <ImageSlider/>
     </div>
   );
 }
--- src/components/imageData.js
+++ src/components/imageData.js
@@ -1,4 +1,7 @@
-import p1 from "../assets/report.png"
+import f1 from "../assets/f1.png"
+import n1 from "../assets/n1.png"
+import n2 from "../assets/n2.png"
+import n3 from "../assets/n3.png"
 
 class cirtification {
     constructor(name, image, description, date) {
@@ -12,8 +15,26 @@ class cirtification {
 export const ImageData = [
     new cirtification(
         "", 
-        p1, 
-        "Fix: device status & battery logic", 
+        f1, 
+        "Fixed: Conect devices API instead of Location API. (Frontend done, Backend done)", 
+        "2022-10-18"
+        ),
+    new cirtification(
+        "", 
+        n1, 
+        "New issue 1 (Frontend done, Backend inprogress)", 
+        "2022-10-18"
+        ),
+    new cirtification(
+        "", 
+        n2, 
+        "New issue 2", 
+        "2022-10-18"
+        ),
+    new cirtification(
+        "", 
+        n3, 
+        "New issue 3", 
         "2022-10-18"
         ),
 ]
