651a21bc48e277c5e47f72916b11d575b174ba10
JamesBond <introbond.dev@gmail.com>
2023-09-07 09:30:56 +0700

    Refactor

--- src/components/imageData.js
+++ src/components/imageData.js
@@ -1,4 +1,4 @@
-import f1 from "../assets/f1.png"
+import f1 from "../assets/flux.png"
 import f2 from "../assets/count-his.png"
 import n1 from "../assets/n1.png"
 import n2 from "../assets/n2.png"
@@ -17,31 +17,7 @@ export const ImageData = [
     new cirtification(
         "", 
         f1, 
-        "Fixed: Conect devices API instead of Location API. (Frontend done, Backend done)", 
-        "2022-10-18"
-        ),
-    new cirtification(
-        "", 
-        f2, 
-        "Fixed: Count history. (Frontend done, Backend done)", 
-        "2022-10-18"
-        ),
-    new cirtification(
-        "", 
-        n2, 
-        "New issue 1 (done)", 
-        "2022-10-18"
-        ),
-    new cirtification(
-        "", 
-        n1, 
-        "New issue 2 (Frontend done, Backend inprogress)", 
-        "2022-10-18"
-        ),
-    new cirtification(
-        "", 
-        n3, 
-        "New issue 3", 
-        "2022-10-18"
+        "Influx scripts for MMAT API", 
+        ""
         ),
 ]
